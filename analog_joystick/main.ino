8a2cb6e0c008fa25c8d7003255ca16b5a134263c
touhf <tobytubar@gmail.com>
2023-04-23 15:06:01 +0500

    analog joystick

---
+++ analog_joystick/main.ino
@@ -0,0 +1,23 @@
+// arduino pin numbers
+const int SW_pin = 2; // digital pin connected to swtich output
+const int X_pin = 0; // analog pin connected to X output
+const int Y_pin = 1; // analog pin connected to Y output
+
+void setup() {
+  pinMode(SW_pin, INPUT);
+  digitalWrite(SW_pin, HIGH);
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.print("Switch: ");
+  Serial.print(digitalRead(SW_pin));
+  Serial.print("\n");
+  Serial.print("X-axis: ");
+  Serial.print(analogRead(X_pin));
+  Serial.print("\n");
+  Serial.print("Y-axis: ");
+  Serial.print(analogRead(Y_pin));
+  Serial.print("\n\n");
+  delay(500);
+}
